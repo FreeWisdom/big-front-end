f1f474de3b1d5f95d4f6bea95a37fd39ef0fc2b2
Thales <1134941536@qq.com>
2023-02-25 16:09:30 +0800

    fix: delete history

--- koa/demo/.history/middeware_20230222065439.js
+++
@@ -1,4 +0,0 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-
-const app = new Koa();
--- koa/demo/.history/middeware_20230222065618.js
+++
@@ -1,9 +0,0 @@
-const Koa = require('koa');
-
-const middeware = async(ctx) => {
-    console.log("ctx");
-}
-
-const app = new Koa();
-
-App.use(middeware);
